c5a62cc9716d47a5a727a7b5f627e978014f436b
overlake333 <taylor.m.olson.21@dartmouth.edu>
2019-04-17 17:25:08 -0400

    code

--- src/youtube-api.js
+++ src/youtube-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://www.googleapis.com/youtube/v3/search';
-const API_KEY = 'YOUR API KEY'; // add your own key here
+const API_KEY = 'AIzaSyDtBfCrmxzdsRxHyI3ifyc6R_qndj0kGsk'; // add your own key here
 
 const youtubeSearch = (term) => {
   const params = {
